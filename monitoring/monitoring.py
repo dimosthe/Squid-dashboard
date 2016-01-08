8347a58dfb9e8ac67b3574d0493f523316ca7811
George <georgios.dimo@gmail.com>
2016-01-08 12:21:25 +0200

    save state in monitoring.py

--- monitoring/monitoring.py
+++ monitoring/monitoring.py
@@ -44,47 +44,62 @@ if __name__ == '__main__':
     squid = Squidclient()
    
     first = True
-    #while True:
+    f = open('/home/proxyvnf/dashboard/Squid-dashboard/monitoring/logs.txt', 'a')
     process = squid.execute()
-    current_http = squid.parse(process, "Number of HTTP requests received:")
-    hits_percentage = squid.parse(process, "Hits as % of all requests:", ",", False)
-    memory_hits = squid.parse(process, "Memory hits as % of hit requests:", ",", False)
-    disk_hits = squid.parse(process, "Disk hits as % of hit requests:", ",", False)
-    cache_disk_utilization = squid.parse(process, "Storage Swap capacity:", ",")
-    cache_memory_utilization = squid.parse(process, "Storage Mem capacity:", ",")
-    number_of_users = squid.parse(process, "Number of clients accessing cache:")
-    cpu_usage = squid.parse(process, "CPU Usage:")
     
-    if all((current_http, hits_percentage, memory_hits, disk_hits, cache_disk_utilization, cache_memory_utilization, number_of_users, cpu_usage)):
-        current_http = int(current_http)
-        if first:
-            previous_http = current_http
-            first = False
-    
-        diff_http = current_http - previous_http # gets the number of HTTP requests since the previous measurement
+    if process:
+        current_http = squid.parse(process, "Number of HTTP requests received:")
+        hits_percentage = squid.parse(process, "Hits as % of all requests:", ",", False)
+        memory_hits = squid.parse(process, "Memory hits as % of hit requests:", ",", False)
+        disk_hits = squid.parse(process, "Disk hits as % of hit requests:", ",", False)
+        cache_disk_utilization = squid.parse(process, "Storage Swap capacity:", ",")
+        cache_memory_utilization = squid.parse(process, "Storage Mem capacity:", ",")
+        number_of_users = squid.parse(process, "Number of clients accessing cache:")
+        cpu_usage = squid.parse(process, "CPU Usage:")
 
-        if diff_http < 0:
-            diff_http = 0
+        if all((current_http, hits_percentage, memory_hits, disk_hits, cache_disk_utilization, cache_memory_utilization, number_of_users, cpu_usage)):
+            current_http = int(current_http)
+            
+            file = open('/home/proxyvnf/dashboard/Squid-dashboard/monitoring/state.txt', 'r')
+            previous_http = int(file.read())
+            file.close()
+            if previous_http == 0:
+                previous_http = current_http
 
-        previous_http = current_http
-        #shell.send_metric('httpnum', diff_http)
-        #shell.send_metric('hits', hits_percentage[:-1])
-        #shell.send_metric('memoryhits', memory_hits[:-1])
-        #shell.send_metric('diskhits', disk_hits[:-1])
-        #shell.send_metric('cachediskutilization', cache_disk_utilization[:-6])
-        #shell.send_metric('cachememkutilization', cache_memory_utilization[:-6])
-        #shell.send_metric('usernum', number_of_users)
-        #shell.send_metric('cpuusage', cpu_usage[:-1])
-        f = open('/home/proxyvnf/dashboard/Squid-dashboard/monitoring/logs.txt', 'a')
+            file = open('/home/proxyvnf/dashboard/Squid-dashboard/monitoring/state.txt', 'w')
+            file.write(str(current_http))
+            file.close()
+            
+            diff_http = current_http - previous_http # gets the number of HTTP requests since the previous measurement
+            if diff_http < 0:
+                diff_http = 0
+
+            #previous_http = current_http
+            #shell.send_metric('httpnum', diff_http)
+            #shell.send_metric('hits', hits_percentage[:-1])
+            #shell.send_metric('memoryhits', memory_hits[:-1])
+            #shell.send_metric('diskhits', disk_hits[:-1])
+            #shell.send_metric('cachediskutilization', cache_disk_utilization[:-6])
+            #shell.send_metric('cachememkutilization', cache_memory_utilization[:-6])
+            #shell.send_metric('usernum', number_of_users)
+            #shell.send_metric('cpuusage', cpu_usage[:-1])
+            f.write("time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"\n")
+            f.write("Number of HTTP requests received: "+str(diff_http)+"\n")
+            f.write("Hits as % of all requests: "+hits_percentage[:-1]+"\n")
+            f.write("Memory hits as % of hit requests: "+memory_hits[:-1]+"\n")
+            f.write("Disk hits as % of hit requests: "+disk_hits[:-1]+"\n")
+            f.write("Storage Swap capacity: "+cache_disk_utilization[:-6]+"\n")
+            f.write("Storage Mem capacity: "+cache_memory_utilization[:-6]+"\n")
+            f.write("Number of clients accessing cache:"+number_of_users+"\n")
+            f.write("CPU Usage: "+cpu_usage[:-1]+"\n")
+            f.write("----------------------\n")
+        else:
+            f.write("time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"\n")
+            f.write('Unable to send metrics\n')
+            f.write("----------------------\n")
+    else:
         f.write("time: "+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"\n")
-        f.write("Number of HTTP requests received: "+str(diff_http)+"\n")
-        f.write("Hits as % of all requests: "+hits_percentage[:-1]+"\n")
-        f.write("Memory hits as % of hit requests: "+memory_hits[:-1]+"\n")
-        f.write("Disk hits as % of hit requests: "+disk_hits[:-1]+"\n")
-        f.write("Storage Swap capacity: "+cache_disk_utilization[:-6]+"\n")
-        f.write("Storage Mem capacity: "+cache_memory_utilization[:-6]+"\n")
-        f.write("Number of clients accessing cache:"+number_of_users+"\n")
-        f.write("CPU Usage: "+cpu_usage[:-1]+"\n")
+        f.write('Unable to send metrics\n')
         f.write("----------------------\n")
-        f.close()
-        #time.sleep(60)
+    
+    f.close()
